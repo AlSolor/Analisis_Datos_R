94caec1d88f7bf58e7ac78b8e5bc1c10cebcc269
AlSolor <alansolora@gmail.com>
2023-08-22 20:16:41 -0600

    Third Exercise

---
+++ CHAPTER_01/Exercise_03.R
@@ -0,0 +1,16 @@
+#Third exercise
+#Calculate the mean and the variance of both sets of numbers
+
+x1 <- c(1,2,3,4,5,6)
+x2 <- c(3,5,1)
+
+mean_x1 <- mean(x1)
+mean_x2 <- mean(x2)
+
+var_x1 <- var(x1)
+var_x2 <- var(x2)
+
+print(mean_x1)
+print(var_x1)
+print(mean_x2)
+print(var_x2)
