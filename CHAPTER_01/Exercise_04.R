643568729f4309d84eb6679c1c71d0176a14db48
AlSolor <alansolora@gmail.com>
2023-09-22 11:31:51 -0600

    Sixth Exercise

--- CHAPTER_01/Exercise_04.R
+++ CHAPTER_01/Exercise_04.R
@@ -3,4 +3,16 @@
 
 x1 <- c(1,2,3,4,5,6)
 
+sum_values <- sum(x1)
+mean_x1 <- sum_values / length(x1)
+mean_x1
 
+
+var_x1 <- ((x1[1]-mean_x1)**2)
+var_x1 <- var_x1 + ((x1[2]-mean_x1)**2)
+var_x1 <- var_x1 + ((x1[3]-mean_x1)**2)
+var_x1 <- var_x1 + ((x1[4]-mean_x1)**2)
+var_x1 <- var_x1 + ((x1[5]-mean_x1)**2)
+var_x1 <- var_x1 + ((x1[6]-mean_x1)**2)
+var_x1 <- var_x1 / length(x1)
+var_x1
