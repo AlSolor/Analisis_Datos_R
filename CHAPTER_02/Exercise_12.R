88081a90863fab7bc58fdd73aaeed65dea2fc0b5
AlSolor <alansolora@gmail.com>
2023-09-25 13:34:00 -0600

    How many subjects would be hired if the company only allows respon scores >= 52

--- CHAPTER_02/Exercise_12.R
+++ CHAPTER_02/Exercise_12.R
@@ -1,8 +1,8 @@
 library("foreign")
 library("modeest")
 library("moments")
-# The company con only hire people with a responsability score above 52 points.
-#How many subjects would be hired?
+# The company con only hire people with a responsability score >= 52 points.
+#How many subjects would be hired if the company only allows respon scores >= 52
 datos <- read.spss('datasets/practica.sav', to.data.frame = TRUE)
 
 #Create a frequency table
@@ -19,7 +19,7 @@ datos <- read.spss('datasets/practica.sav', to.data.frame = TRUE)
 subjects <- data.frame(t1,t3,t2,t4)
 
 #We select the amount of subjects with a score of 52 or below
-subject_below_52 <- subset(subjects,Var1 == '52')$t3
+subject_below_52 <- subset(subjects,Var1 == '51')$t3
 
 
 #Then we select the total subjects
