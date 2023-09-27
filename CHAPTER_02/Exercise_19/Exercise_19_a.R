9f3fdf181f0198647e71e3d5ae42c05668e56f55
AlSolor <alansolora@gmail.com>
2023-09-27 10:08:08 -0600

    Obtener la varianza y la media de estatura y peso en escala norteamericana

--- CHAPTER_02/Exercise_19/Exercise_19_a.R
+++ CHAPTER_02/Exercise_19/Exercise_19_a.R
@@ -2,6 +2,22 @@ library("foreign")
 library("modeest")
 library("moments")
 
-#
+#Inciso a
+#Obtener la varianza y la media de estatura y peso en escala norteamericana
+
 datos <- read.spss('datasets/practica.sav', to.data.frame = TRUE)
+summary(datos$peso)
+
+#Transformando la altura a pulgadas
+datos$estatura <- (datos$estatura * 100)/2.54
+
+#Transformanda el peso a libras
+datos$peso <- (datos$peso * 1000)/453.6
+
+#Obteniendo la media y la varianza
+median(datos$estatura)
+var(datos$estatura)
+
+median(datos$peso)
+var(datos$peso)
 
